8b2619d3576be4ca40a5e9a260a40f1dbedcd4c1
Matthew Todd <matthew.todd@gmail.com>
2010-03-13 13:45:38 +0300

    Run tests in tmpdir with PATH set.

--- test/edge_to_edge/system_test.rb
+++ test/edge_to_edge/system_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class SystemTest < ActiveSupport::TestCase
   test 'running the application generates an atom feed' do
-    flunk
+    system 'feeds'
+    assert_file_exists('public/feeds.atom')
+  end
+
+  private
+
+  def assert_file_exists(path)
+    assert File.exist?(path), "A file named #{path} should exist"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,68 @@
 require 'test/unit'
-require 'active_support/backtrace_cleaner'
+require 'active_support'
 require 'active_support/test_case'
 
 if $stdout.tty?
   require 'redgreen'
 end
 
+# ---------------------------------------------------------------------------
+# Add our bin to the PATH so tests can run `feeds`
+# ---------------------------------------------------------------------------
+class Path
+  def initialize(string=ENV['PATH'])
+    @elements = string.split(File::PATH_SEPARATOR)
+  end
+
+  def prepend_relative(path, from=__FILE__)
+    @elements.unshift File.expand_path(path, from)
+    self
+  end
+
+  def clean!
+    @elements.map! { |path| File.expand_path(path) }
+    @elements.uniq!
+    self
+  end
+
+  def to_s
+    @elements.join(File::PATH_SEPARATOR)
+  end
+end
+
+path = Path.new(ENV['PATH'])
+path.prepend_relative('../../bin')
+path.clean!
+ENV['PATH'] = path.to_s
+
+
+# ---------------------------------------------------------------------------
+# Run tests in separate temp directories
+# ---------------------------------------------------------------------------
+require 'tempfile'
+
+module TemporaryTestDirectories
+  def self.included(klass)
+    klass.send :alias_method, :test_without_temporary_directory, :test
+    klass.send :alias_method, :test, :test_with_temporary_directory
+  end
+
+  def test_with_temporary_directory(name, &block)
+    test_without_temporary_directory(name) do
+      Dir.mktmpdir { |tmpdir| Dir.chdir(tmpdir) { send(name) } }
+    end
+
+    define_method(name, &block)
+  end
+end
+
+ActiveSupport::Testing::Declarative.module_eval do
+  include TemporaryTestDirectories
+end
+
+# ---------------------------------------------------------------------------
+# Clean up the backtraces a little bit
+# ---------------------------------------------------------------------------
 class BacktraceFilter < ActiveSupport::BacktraceCleaner
   def initialize
     super
@@ -13,8 +70,11 @@ class BacktraceFilter < ActiveSupport::BacktraceCleaner
   end
 
   def customize
-    add_filter { |line| line.sub('./', '/') }
-    add_filter { |line| line.sub(%r{^.*vendor/gems/ruby/1\.8/gems/(\w+)-([0-9.]+)/}, '\1 (\2) ') }
+    add_filter   { |line| line.sub('./test/', '') }
+    add_filter   { |line| line.sub(%r{^.*/gems/(\w+)-([0-9.]+)/}, '\1 (\2) ') }
+    add_silencer { |line| line =~ /#{Config::CONFIG['rubylibdir']}/ }
+    add_silencer { |line| line =~ /active_support/ }
+    add_silencer { |line| line =~ /test_helper/ }
   end
 
   module TestUnitEnhancements
