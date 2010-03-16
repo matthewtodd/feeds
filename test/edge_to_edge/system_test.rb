6b14b41d1b80f8e2245aed24d3081fc0fab650b8
Matthew Todd <matthew.todd@gmail.com>
2010-03-16 16:38:08 +0300

    Validate the combined feed in the tests.

--- test/edge_to_edge/system_test.rb
+++ test/edge_to_edge/system_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'open3'
 
 class SystemTest < ActiveSupport::TestCase
   test 'running the application generates an atom feed' do
@@ -9,10 +10,9 @@ class SystemTest < ActiveSupport::TestCase
   private
 
   def assert_feed_valid(path)
-    assert_file_exists(path)
-  end
-
-  def assert_file_exists(path)
-    assert File.exist?(path), "A file named #{path} should exist"
+    Open3.popen3('xmllint', '--relaxng', ATOM_RELAX_NG_SCHEMA, path) do |_, stdout, stderr|
+      errors = stderr.readlines
+      assert errors.empty?, errors.join
+    end
   end
 end
